a5605b33c84c2d476f98de866866e618b5758792
Jonathon Chew <jonchew626@hotmail.com>
2025-09-14 18:32:15 +0100

    BATMAN

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	Aphrodite "github.com/jonathon-chew/Aphrodite"
+)
+
+type LanguageStats struct {
+	Name  string
+	Files int
+	Lines int
+}
+
+type Results struct {
+	LanguageStats []LanguageStats
+}
+
+var extToLang = map[string]string{
+	"py":    "Python",
+	"js":    "JavaScript",
+	"java":  "Java",
+	"go":    "Golang",
+	"rs":    "Rust",
+	"cpp":   "C++",
+	"cc":    "C++",
+	"cxx":   "C++",
+	"c":     "C",
+	"cs":    "C#",
+	"php":   "PHP",
+	"rb":    "Ruby",
+	"ts":    "TypeScript",
+	"swift": "Swift",
+	"kt":    "Kotlin",
+	"scala": "Scala",
+	"r":     "R",
+	"dart":  "Dart",
+	"hs":    "Haskell",
+	"m":     "Objective-C",
+	"qml":   "QML",
+	"jl":    "Julia",
+	"sh":    "Shell",
+	"pl":    "Perl",
+	"lua":   "Lua",
+	"sql":   "SQL",
+	"mod":   "Golang",
+	"sum":   "Golang",
+	"html":  "HTML",
+	"ccs":   "CCS",
+}
+
+func HumanReadableInt(i int) string {
+	var humanReadbleNumber string
+	convertedNumber := strconv.Itoa(i)
+
+	if len(convertedNumber) > 3 {
+		for i, v := range convertedNumber {
+			if i%3 == 0 && i != 0 {
+				humanReadbleNumber += ","
+			}
+			humanReadbleNumber += string(v)
+		}
+	} else {
+		humanReadbleNumber = convertedNumber
+	}
+
+	return humanReadbleNumber
+}
+
+func main() {
+	root := "./"
+	var LangStats Results
+	filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil || d.IsDir() {
+			return err // stop on error
+		}
+
+		PointerToFile, OpenFileError := os.Open(path)
+		if OpenFileError != nil {
+			log.Printf("error opening the file %s", path)
+			return nil
+		}
+
+		defer PointerToFile.Close()
+
+		var lines int
+		scanner := bufio.NewScanner(PointerToFile)
+		for scanner.Scan() {
+			if scanner.Text() != "\n" {
+				lines++
+			}
+		}
+
+		fileExtension := strings.ReplaceAll(filepath.Ext(d.Name()), ".", "")
+
+		_, isMapContainsKey := extToLang[fileExtension]
+
+		if strings.Contains(filepath.Ext(d.Name()), ".") && isMapContainsKey {
+
+			var found bool = false
+			for i := range LangStats.LanguageStats {
+				if LangStats.LanguageStats[i].Name == extToLang[fileExtension] {
+					LangStats.LanguageStats[i].Files += 1
+					LangStats.LanguageStats[i].Lines += lines
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				LangStats.LanguageStats = append(LangStats.LanguageStats, LanguageStats{
+					Name:  extToLang[fileExtension],
+					Files: 1,
+					Lines: lines,
+				})
+			}
+
+		}
+
+		return nil
+	})
+
+	for _, printresult := range LangStats.LanguageStats {
+		Aphrodite.PrintColour("Green", fmt.Sprintf("\nName: %s,\nNumber of files: %s,\nNumber of Lines: %s\n\n", printresult.Name, HumanReadableInt(printresult.Files), HumanReadableInt(printresult.Lines)))
+	}
+}
